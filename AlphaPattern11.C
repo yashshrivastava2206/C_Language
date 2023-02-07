00944580bd4ce29998fd576f5d692aca79947cc7
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-02-07 07:46:22 +0530

    Aphabet inverted half pyramid pattern (Type-1)
    WAP to print the alphabet inverted half pyramid pattern 
        A
       BB
      CCC
     DDDD
    EEEEE

---
+++ AlphaPattern11.C
@@ -0,0 +1,30 @@
+/* WAP to print the alphabet inverted half pyramid pattern 
+    A
+   BB
+  CCC
+ DDDD
+EEEEE
+*/
+#include<stdio.h>
+int main()
+{
+    char ch='A';
+    for(int i=5;i>=1;i--)
+    {
+        
+        for( int j=1;j<=5;j++)
+        {
+            if(i<=j)
+            {
+                printf("%c",ch);
+                
+            }
+            else
+            printf(" ");
+            
+        }
+        printf("\n");
+        ch++;
+    }
+    return 0;
+}
