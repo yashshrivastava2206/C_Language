36186cdc57a33d08fee71fdaf99ad79bec4ae318
Yash Shrivastava <118923726+yashshrivastava2206@users.noreply.github.com>
2023-02-14 07:17:39 +0530

    Prepare>C>Functions>PermutationsofStrings
    Strings are usually ordered in lexicographical order. That means they are ordered by comparing their leftmost different characters. For example,  because . Also  because . If one string is an exact prefix of the other it is lexicographically smaller, e.g., .
    
    Given an array of strings sorted in lexicographical order, print all of its permutations in strict lexicographical order. If two permutations look the same, only print one of them.

---
+++ Hacker Rank/PermutationsOfStrings.C
@@ -0,0 +1,71 @@
+/* Strings are usually ordered in lexicographical order. That means they are ordered by comparing their leftmost different characters. For example,  because . Also  because . If one string is an exact prefix of the other it is lexicographically smaller, e.g., .
+
+Given an array of strings sorted in lexicographical order, print all of its permutations in strict lexicographical order. If two permutations look the same, only print one of them. */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void swap(char **s, int i, int j)
+{
+    char * tmp = s[i];
+    s[i] = s[j];
+    s[j] = tmp;
+}
+
+void reverse(char **s, int start, int end)
+{
+    while(start<end)
+    {
+        swap(s,start++,end--);
+    }
+}
+
+int next_permutation(int n, char **s)
+{
+    /**
+    * Complete this method
+    * Return 0 when there is no next permutation and 1 otherwise
+    * Modify array s to its next permutation
+    */
+    for(int i=n-2;i>-1;i--)
+    {
+        if(strcmp(s[i+1],s[i])>0)
+        {
+            //get min max
+            for(int j=n-1;j>i;j--)
+            {
+                if(strcmp(s[j],s[i])>0)
+                {
+                    //do swap
+                    swap(s,i,j);
+                    // do reverse
+                    reverse(s,i+1,n-1);
+                    return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+int main()
+{
+	char **s;
+	int n;
+	scanf("%d", &n);
+	s = calloc(n, sizeof(char*));
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = calloc(11, sizeof(char));
+		scanf("%s", s[i]);
+	}
+	do
+	{
+		for (int i = 0; i < n; i++)
+			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	} while (next_permutation(n, s));
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
+	return 0;
+}
